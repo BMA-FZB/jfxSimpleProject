ba63e87fc6634d00812dd24093f4bc6b9b5da0e6
AminBouchareb <93881530+boucharebea@users.noreply.github.com>
2023-12-07 12:56:41 +0100

    Update Launcher.java

--- src/main/java/com/example/demo/Launcher.java
+++ src/main/java/com/example/demo/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("hello every one ");
         Application.launch(HelloApplication.class,args);
 
     }
