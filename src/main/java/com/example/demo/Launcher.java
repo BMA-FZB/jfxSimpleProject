22b0a8b70ad9f3dc9fdb4703e5e901ef8831983b
AminBouchareb <93881530+boucharebea@users.noreply.github.com>
2023-12-07 13:02:52 +0100

    first commit

--- src/main/java/com/example/demo/Launcher.java
+++ src/main/java/com/example/demo/Launcher.java
@@ -3,9 +3,9 @@ package com.example.demo;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("hello every one ");
-        Application.launch(HelloApplication.class,args);
+	public static void main( String[] args ){
+		System.out.println( "hello every one " );
+		Application.launch( HelloApplication.class, args );
 
-    }
+	}
 }
